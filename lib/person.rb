4d05feaca3bcd00f0337fcd9c7e2a183ec8fe028
Kevin Nielsen <knielsen24@gmail.com>
2022-06-08 16:15:12 -0700

    commit tree 627de7145950a8c6db4537ee7f0aee5aa7909512

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Person
-  # your code here
+
+	def initialize (attributes)
+		attributes.each do |key, value|
+			self.class.attr_accessor(key)
+			self.send("#{key}=", value)
+		end
+	end
 end
+
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+def reload
+	load "lib/person.rb"
+end
+
+binding.pry
